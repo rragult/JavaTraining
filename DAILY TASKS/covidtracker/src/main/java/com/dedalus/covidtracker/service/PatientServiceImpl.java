23c308b61759f8891c02fee1814c4af1598c5fae
Ragul <81378069+rragult@users.noreply.github.com>
2023-05-15 21:59:37 +0530

    Update PatientServiceImpl.java

--- DAILY TASKS/covidtracker/src/main/java/com/dedalus/covidtracker/service/PatientServiceImpl.java
+++ DAILY TASKS/covidtracker/src/main/java/com/dedalus/covidtracker/service/PatientServiceImpl.java
@@ -1,84 +1,40 @@
 package com.dedalus.covidtracker.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
-import com.dedalus.covidtracker.entity.Patient;
-import com.dedalus.covidtracker.exception.PatientAlreadyExistsException;
-import com.dedalus.covidtracker.exception.PatientNotFoundException;
-import com.dedalus.covidtracker.repository.PatientRepository;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.dedalus.covidtracker.dto.StateStats;
+import com.dedalus.covidtracker.helper.DBHelper;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class PatientServiceImpl implements PatientService{
-	private PatientRepository patientRepository;
-	
+public class CovidStatsServiceImpl implements CovidStatsService {	
 	@Override
-	public Patient createPatient(Patient patient) throws PatientAlreadyExistsException{
-		if(patientRepository.findByAadhaarno(patient.getAadhaarno()).isPresent())
-		{
-			throw new PatientAlreadyExistsException();
-		}
+	public List<StateStats> getStatewiseStats(String countryCode) {		
+		//SimpleJdbcCall actor = new SimpleJdbcCall(DBHelper.getDataSource()).withProcedureName("getStatewiseStats");
+		SimpleJdbcCall actor = new SimpleJdbcCall(DBHelper.getDataSource()).withProcedureName("getStatewiseStats").returningResultSet("statscur", BeanPropertyRowMapper.newInstance(StateStats.class));
+		SqlParameterSource params = new MapSqlParameterSource().addValue("p_countrycode", countryCode);	
+		//SqlParameterSource params = new MapSqlParameterSource().addValue("statscur", , OracleTypes.CURSOR, new SomeRowMapper());			
+		Map<String, Object> out = actor.execute(params);
+		
+		//return StateStats.map(out, 1);
+		if(out.get("statscur") == null)
+			return new ArrayList<StateStats>();
 		else
-		{
-			return patientRepository.save(patient);
-		}
-	}
-
-	@Override
-	public Patient getPatientById(long patientId) {
-		Optional<Patient> optionalPatient = patientRepository.findById(patientId);
-		return optionalPatient.get();
-	}
-
-	@Override
-	public List<Patient> getAllPatients() {		
-		return patientRepository.findAll(Sort.by(Sort.Direction.ASC,"patientfirstname"));
-	}
-
-	@Override
-	public Patient updatePatient(Patient patient) throws PatientNotFoundException{
-		if(patientRepository.findById(patient.getPatientid()).isEmpty())
-		{
-			throw new PatientNotFoundException();
-		}
-		else
-		{
-			Patient existingPatient = patientRepository.findById(patient.getPatientid()).get();
-			existingPatient.setPatientfirstname(patient.getPatientfirstname());
-			existingPatient.setPatientlastname(patient.getPatientlastname());
-			existingPatient.setPatientdob(patient.getPatientdob());
-			existingPatient.setPatientgender(patient.getPatientgender());
-			existingPatient.setPatientcontactno(patient.getPatientcontactno());
-			existingPatient.setPatientemailid(patient.getPatientemailid());
-			existingPatient.setPatientaddr1(patient.getPatientaddr1());
-			existingPatient.setPatientaddr2(patient.getPatientaddr2());
-			existingPatient.setStatecode(patient.getStatecode());
-			existingPatient.setPostalcode(patient.getPostalcode());
-			existingPatient.setPatientstatuscode(patient.getPatientstatuscode());		
-			existingPatient.setAadhaarno(patient.getAadhaarno());
-			return patientRepository.save(existingPatient);
-		}
-	}
-
-	@Override
-	public void deletePatient(Long patientId) throws PatientNotFoundException{
-		if(patientRepository.findById(patientId).isEmpty())
-		{
-			throw new PatientNotFoundException();
-		}
-		else
-		{
-			Patient existingPatient = patientRepository.findById(patientId).get();
-			existingPatient.setActiveind('D');
-			patientRepository.save(existingPatient);
-		}
+			return (List<StateStats>) out.get("statscur");
 	}
 
 }
